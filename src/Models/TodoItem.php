6352f8eb484b113f2b3af0ab9cda38f484985da1
MohamedAbouDabous <mohamed.abou-dabous@chasacademy.se>
2019-01-18 14:35:17 +0100

    Lade till funktionendeleteTodo i todoitem executar en query. Samt funktionen delete la jag t ill i TodoController som redirectar mig till första sidan om $result är sant

---
+++ src/Models/TodoItem.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Todo;
+
+class TodoItem extends Model
+{
+    const TABLENAME = 'todos'; // This is used by the abstract model, don't touch
+
+    public static function createTodo($title)
+    {
+        $query = "INSERT INTO todo ($title)"
+
+
+        self::$db->query($query);
+        $result = self::$statement->execute();
+
+        
+
+
+    }
+
+    public static function updateTodo($todoId, $title, $completed = null)
+    {
+
+    }
+
+    public static function deleteTodo($todoId)
+    {
+        $query = "DELETE FROM todos WHERE id = $todoId";
+        
+        self::$db->query($query);
+
+        $result = self::$db->execute();
+        
+        return $result;
+    }
+
+
+
+    // public static function createTodo($title)
+    // {
+    //     // TODO: Implement me!
+    //     // Create a new todo
+    // }
+
+    // // public static function updateTodo($todoId, $title, $completed = null)
+    // // {
+    // //     // TODO: Implement me!
+    // //     // Update a specific todo
+    // // }
+
+    // public static function deleteTodo($todoId)
+    // {
+    //     // TODO: Implement me!
+    //     // Delete a specific todo
+    // }
+    
+    // (Optional bonus methods below)
+    // public static function toggleTodos($completed)
+    // {
+    //     // TODO: Implement me!
+    //     // This is to toggle all todos either as completed or not completed
+    // }
+
+    // public static function clearCompletedTodos()
+    // {
+    //     // TODO: Implement me!
+    //     // This is to delete all the completed todos from the database
+    // }
+
+}
---
+++ src/routes.php
@@ -0,0 +1,13 @@
+<?php
+
+$router->get('', 'TodoController@get');
+
+$router->post('todos', 'TodoController@add');
+
+$router->patch('todos/{id}', 'TodoController@update');
+
+$router->get('todos/{id}/delete', 'TodoController@delete');
+
+$router->post('todos/toggle-all', 'TodoController@toggle');
+
+$router->post('todos/clear-completed', 'TodoController@clear');
